4dccb709455fc038ff583539fa6c81daa9b8b2d5
Nagaaswin <nagaaswin775@gmail.com>
2020-05-11 13:50:47 +0530

    Started Learning through course

--- javaScript/JavaScript Functions.js
+++ javaScript/JavaScript Functions.js
@@ -1,49 +1,49 @@
 function toCelsius(f) {
-  return (5/9) * (f-32);
+  return (5 / 9) * (f - 32);
 }
 document.getElementById("demo").innerHTML = toCelsius(77);
 document.getElementById("demo1").innerHTML = toCelsius;
 
 var person = {
-  fullName: function() {
+  fullName: function () {
     return this.firstName + " " + this.lastName;
-  }
-}
+  },
+};
 var person1 = {
-  firstName:"John",
-  lastName: "Doe"
-}
+  firstName: "John",
+  lastName: "Doe",
+};
 var person2 = {
-  firstName:"Mary",
-  lastName: "Doe"
-}
+  firstName: "Mary",
+  lastName: "Doe",
+};
 var x = person.fullName.call(person1);
 document.getElementById("demo2").innerHTML = x;
 
 var person = {
-  fullName: function(city, country) {
+  fullName: function (city, country) {
     return this.firstName + " " + this.lastName + "," + city + "," + country;
-  }
-}
+  },
+};
 var person1 = {
-  firstName:"John",
-  lastName: "Doe"
-}
+  firstName: "John",
+  lastName: "Doe",
+};
 var person2 = {
-  firstName:"Mary",
-  lastName: "Doe"
-}
+  firstName: "Mary",
+  lastName: "Doe",
+};
 var x = person.fullName.call(person1, "Oslo", "Norway");
 document.getElementById("demo3").innerHTML = x;
 
 var person = {
-  fullName: function(city, country) {
+  fullName: function (city, country) {
     return this.firstName + " " + this.lastName + "," + city + "," + country;
-  }
-}
+  },
+};
 var person1 = {
-  firstName:"John",
-  lastName: "Doe"
-}
+  firstName: "John",
+  lastName: "Doe",
+};
 var x = person.fullName.apply(person1, ["Oslo", "Norway"]);
-document.getElementById("demo4").innerHTML = x; 
+document.getElementById("demo4").innerHTML = x;
