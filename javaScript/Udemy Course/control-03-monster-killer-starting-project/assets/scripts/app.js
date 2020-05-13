ea066799a8113eecc823c0b64fb4cf698bf936ea
Nagaaswin <nagaaswin775@gmail.com>
2020-05-13 19:45:50 +0530

    conditional statments learnt and used.

---
+++ javaScript/Udemy Course/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -0,0 +1,155 @@
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 15;
+const MONSTER_ATTACK_VALUE = 17;
+const HEAL_VALUE = 20;
+
+const MODE_ATTACK = 'attack';
+const MODE_STRONG_ATTACK = 'strongAttack';
+const LOG_EVENT_PLAYER_ATTACK = 'playerAttack';
+const LOG_EVENT_PLAYER_STRONG_ATTACK = 'playerStrongAttack';
+const LOG_EVENT_MONSTER_ATTACK = 'monsterAttack';
+const LOG_EVENT_PLAYER_HEAL = 'playerHeal';
+const LOG_EVENT_GAME_OVER = 'gameOver';
+
+const enteredNumber = prompt('Max life for your monster and you.', '100');
+
+let chosenMaxLife = parseInt(enteredNumber);
+let battleLog = [];
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+  chosenMaxLife = 100;
+}
+
+let currentMonsterHealth = chosenMaxLife;
+let currenPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
+
+adjustHealthBars(chosenMaxLife);
+
+function writeToLog(eve, val, monsterHealth, playerHealth) {
+  let logentry = {
+    event: eve,
+    value: val,
+    finalMonsterHealth: monsterHealth,
+    finalPlayerHealth: playerHealth,
+  };
+
+  switch (eve) {
+    case LOG_EVENT_PLAYER_ATTACK:
+      logentry.target = 'Monster';
+      break;
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logentry.target = 'Monster';
+      break;
+    case LOG_EVENT_MONSTER_ATTACK:
+      logentry.target = 'Player';
+      break;
+    case LOG_EVENT_PLAYER_HEAL:
+      logentry.target = 'Player';
+  }
+  battleLog.push(logentry);
+}
+
+function reset() {
+  currenPlayerHealth = chosenMaxLife;
+  currentMonsterHealth = chosenMaxLife;
+  resetGame(chosenMaxLife);
+}
+
+function endRound() {
+  const initialPlayerHealth = currenPlayerHealth;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currenPlayerHealth -= playerDamage;
+  writeToLog(
+    LOG_EVENT_MONSTER_ATTACK,
+    playerDamage,
+    currentMonsterHealth,
+    currenPlayerHealth
+  );
+
+  if (currenPlayerHealth <= 0 && hasBonusLife) {
+    currenPlayerHealth = initialPlayerHealth;
+    hasBonusLife = false;
+    removeBonusLife();
+    setPlayerHealth(initialPlayerHealth);
+    alert('You would have been dead but bonus life saved you!');
+  }
+
+  if (currentMonsterHealth <= 0 && currenPlayerHealth > 0) {
+    alert('You Won!');
+    writeToLog(
+      LOG_EVENT_GAME_OVER,
+      'Player won',
+      currentMonsterHealth,
+      currenPlayerHealth
+    );
+  } else if (currenPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You Lost! ');
+    writeToLog(
+      LOG_EVENT_GAME_OVER,
+      'Player Lost',
+      currentMonsterHealth,
+      currenPlayerHealth
+    );
+  } else if (currentMonsterHealth <= 0 && currenPlayerHealth <= 0) {
+    alert('You have draw! ');
+    writeToLog(
+      LOG_EVENT_GAME_OVER,
+      'A draw match',
+      currentMonsterHealth,
+      currenPlayerHealth
+    );
+  }
+
+  if (currentMonsterHealth <= 0 || currenPlayerHealth <= 0) {
+    reset();
+  }
+}
+
+function attackMonster(mode) {
+  const maxDamage = mode === MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
+  const logevent =
+    mode === MODE_ATTACK
+      ? LOG_EVENT_PLAYER_ATTACK
+      : LOG_EVENT_PLAYER_STRONG_ATTACK;
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  writeToLog(logevent, damage, currentMonsterHealth, currenPlayerHealth);
+  endRound();
+}
+
+function attackHandler() {
+  attackMonster(MODE_ATTACK);
+}
+
+function strongAttackHandler() {
+  attackMonster(MODE_STRONG_ATTACK);
+}
+
+function healPlayerHandler() {
+  let heal;
+  if (currenPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert("You can't heal to more than your max initial health.");
+    heal = chosenMaxLife - currenPlayerHealth;
+  } else {
+    heal = HEAL_VALUE;
+  }
+  increasePlayerHealth(heal);
+  currenPlayerHealth += heal;
+  writeToLog(
+    LOG_EVENT_PLAYER_HEAL,
+    heal,
+    currentMonsterHealth,
+    currenPlayerHealth
+  );
+  endRound();
+}
+
+function showLogHandler() {
+  console.log(battleLog);
+}
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
+logBtn.addEventListener('click', showLogHandler);
