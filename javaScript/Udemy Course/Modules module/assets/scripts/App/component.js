44823be5e148c6475ef67f80969f4b0851b0027c
Nagaaswin <Nagaaswin775@gmail.com>
2020-08-03 21:54:57 +0530

    Completed the Modules module

---
+++ javaScript/Udemy Course/Modules module/assets/scripts/App/component.js
@@ -0,0 +1,27 @@
+export function doNothing() {}
+
+/* Creating a default class*/
+export default class {
+  constructor(hostElementId, insertBefore = false) {
+    if (hostElementId) {
+      this.hostElement = document.getElementById(hostElementId);
+    } else {
+      this.hostElement = document.body;
+    }
+    this.insertBefore = insertBefore;
+  }
+
+  detach() {
+    if (this.element) {
+      this.element.remove();
+      // this.element.parentElement.removeChild(this.element);
+    }
+  }
+
+  attach() {
+    this.hostElement.insertAdjacentElement(
+      this.insertBefore ? 'afterbegin' : 'beforeend',
+      this.element
+    );
+  }
+}
