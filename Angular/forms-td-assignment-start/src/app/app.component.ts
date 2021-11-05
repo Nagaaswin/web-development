552ca0122ebec47fc2c5ed274131111fd3bdd841
Nagaaswin S <nagaaswin775@gmail.com>
2021-11-05 09:50:13 +0530

    Started reactive forms

--- Angular/forms-td-assignment-start/src/app/app.component.ts
+++ Angular/forms-td-assignment-start/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { Form, NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  @ViewChild("formData") formData: NgForm;
 
+  onSubmit() {
+    console.log(this.formData.value);
+  }
 }
